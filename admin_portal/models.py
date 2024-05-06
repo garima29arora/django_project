a889d0f7828d5cdff53f685ebafe568fd99744db
garima29arora <168801802+garima29arora@users.noreply.github.com>
2024-05-06 19:13:54 +0530

    Add files via upload

---
+++ admin_portal/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, employee_id, password=None, **extra_fields):
+        if not employee_id:
+            raise ValueError("The Employee ID field is required")
+        user = self.model(employee_id=employee_id, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, employee_id, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(employee_id, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    employee_id = models.CharField(max_length=30, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'employee_id'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.employee_id
